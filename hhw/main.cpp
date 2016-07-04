3988f1b03f47411168043da7708ceb77b11ed623
GitHG22 <hg1229@gmail.com>
2016-07-04 22:14:40 +0530

    First.Commit.Ever

---
+++ hhw/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n1;
+    int n2;
+    cout<<"Enter the numbers:";
+    cout<<"\nN1=";
+    cin>>n1;
+    cout<<"N2=";
+    cin>>n2;
+    char op;
+    cout<<"Which Operation? : ";
+    cin>>op;
+
+    switch(op)  {
+    case '+': cout<<n1<<'+'<<n2<<'='<<n1+n2; break;
+    case '-': cout<<n1<<'-'<<n2<<'='<<n1-n2; break;
+    case '/': cout<<n1<<'/'<<n2<<'='<<n1/n2; break;
+    case '*': cout<<n1<<'*'<<n2<<'='<<n1*n2; break;
+    default : cout<<"Wat?";
+    }
+
+    return 0;
+}
