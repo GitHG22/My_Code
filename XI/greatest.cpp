48715c2d7b1f84d09f90f75d463196f81de409fb
Nalin Shani <nalinshani14@gmail.com>
2016-07-31 21:37:58 +0530

    Update greatest.cpp

--- XI/greatest.cpp
+++ XI/greatest.cpp
@@ -1,114 +1,25 @@
 #include<iostream>
-#include<windows.h>
-
+#include<conio.h>
+#include<stdlib.h>
 using namespace std;
-
-void greatest(){
-  int n;
-  cout<<"How many numbers you want to be compared? ";
-  cin>>n;
-  cout<<'\n';
-  if (n>1&&n<5){
-    int a;
-    int b;
-    if(n==2){
-      cout<<"Enter the first number ";
-      cin>>a;
-      cout<<"Enter the second number ";
-      cin>>b;
-      if(a==b){
-        cout<<"The numbers are equal :/";
-      }
-      else {
-      if(a>b) {
-        cout<<a<<" is greatest";
-      }
-      else  {
-        cout<<b<<" is greatest";
-      }
+int main()
+{
+    int x,mx,a,mn;
+    cout<<"Enter the numbers| 0to exit\n";
+    cin>>x;
+    if(x!=0){
+     mx=x;
+     mn=x;
+    while(a!=0)
+    {
+        cin>>a;
+        if(a!=0){
+        if(a>mx)mx=a;
+        if(a<mn)mn=a;}
     }
+    cout<<"Max=" <<mx;
+    cout<<"\nMin=" <<mn;
     }
-    else if(n==3){
-      int c;
-      cout<<"Enter the first number ";
-      cin>>a;
-      cout<<"Enter the second number ";
-      cin>>b;
-      cout<<"Enter the third number ";
-      cin>>c;
-      if(a==b&&b==c){
-        cout<<"The numbers are equal :/";
-      }
-      else{
-      if(a>=b) {
-        if(a>=c){
-          cout<<a<<" is greatest";
-        }
-        else {
-          cout<<c<<" is greatest";
-        }
-      }
-      else {
-        if(b>=c){
-          cout<<b<<" is greatest";
-        }
-        else{
-          cout<<c<<" is greatest";
-        }
-      }
-    }
-    }
-    else {
-      int c;
-      int d;
-      cout<<"Enter the first number ";
-      cin>>a;
-      cout<<"Enter the second number ";
-      cin>>b;
-      cout<<"Enter the third number ";
-      cin>>c;
-      cout<<"Enter the fourth number ";
-      cin>>d;
-      if(a==b&&b==c&&b==d){
-        cout<<"The numbers are equal :/";
-      }
-      else{
-      if(a>=b){
-        if(a>=c){
-          if(a>=d){
-            cout<<a<<" is greatest";
-          }
-          else {
-            cout<<d<<" is greatest";
-          }
-        }
-        else if(c>=d){
-          cout<<c<<" is greatest";
-        }
-        else {
-          cout<<d<<" is greatest";
-        }
-      }
-      else if(b>=c){
-        if(b>=d){
-          cout<<b<<" is greatest";
-        }
-        else{
-          cout<<d<<" is greatest";
-        }
-      }
-      else if(c>=d){
-        cout<<c<<" is greatest";
-      }
-      else{
-        cout<<d<<" is greatest";
-      }
-    }
-    }
-}
-  else {
-    cout<<"Duuuuuuude";
-    exit(1);
-  }
-
+    else
+        exit(0);
 }
